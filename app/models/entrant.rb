82c865bed8894bfcada8c5fed17e54f06b5adf57
BegoUrsus <layira2002@gmail.com>
2016-05-15 13:23:41 +0200

    finished 'Implement racers/result Cross-Collection Relationships'

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -6,6 +6,7 @@ class Entrant
 
 	embeds_many :results, class_name: "LegResult", order: [:"event.o".asc], after_add: :update_total
 	embeds_one :race, class_name: "RaceRef"
+	embeds_one :racer, class_name: "RacerInfo", as: :parent
 
   field :bib, type: Integer
   field :secs, type: Float
